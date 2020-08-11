5db0aec4127f6a1250e606b00652818dcd0f33e7
Max Arellano <maxfrda@gmail.com>
2020-08-11 00:01:35 -0400

    dropdown functioning

--- home.js
+++ home.js
@@ -1,4 +1,8 @@
 const icons = document.getElementsByClassName('minus');
+const dropdown = document.getElementById('extras-dropdown');
+const plusMinus = document.getElementById('plus-minus');
+const minus = '<i class="fas fa-minus fa-2x"></i>';
+const plus = '<i class="fas fa-plus fa-2x"></i>';
 
 function setListeners(item) {
   item.addEventListener('click', () => {
@@ -8,8 +12,17 @@ function setListeners(item) {
 }
 [...icons].forEach(setListeners);
 
-// var testIcon = document.querySelector('.minus');
+plusMinus.addEventListener('click', () => {
+  flipPlans(dropdown);
+});
 
-// testIcon.addEventListener('click', () =>{
-//   testIcon.parentNode.style.display = 'none';
-// })
+function flipPlans(item) {
+  const element = item;
+  if (element.style.display !== 'block') {
+    element.style.display = 'block';
+    plusMinus.innerHTML = minus;
+  } else {
+    element.style.display = 'none';
+    plusMinus.innerHTML = plus;
+  }
+}
