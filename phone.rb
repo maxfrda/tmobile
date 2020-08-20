25d3352041650759e265c805a1da0d726d8f719d
Max Arellano <maxfrda@gmail.com>
2020-08-19 22:57:28 -0400

    drag

--- phone.rb
+++ phone.rb
@@ -1,7 +1,9 @@
 class Phone
+  attr_reader :image
   def initialize(attributes = {})
     @price = attributes[:price]
     @name = attributes[:name]
+    @down = attributes[:down] || 0
     @trade_in = attributes[:trade_in] || false
     @bogo = attributes[:bogo] || false
     @aal = attributes[:aal] || false
@@ -16,6 +18,7 @@ end
 
 
 
-iphone11 = Phone.new(name: 'iPhone 11', price: 699.99, bogo: true)
+iphone11 = Phone.new(name: 'iPhone 11', price: 699.99, bogo: true,
+ image: "https://cdn.tmobile.com/content/dam/t-mobile/en-p/cell-phones/OnePlus/OnePlus-8-5G/Interstellar-Glow/OnePlus-8-5G-Interstellar-Glow-frontimage.png")
 
-puts iphone11.eip
+puts iphone11.image
